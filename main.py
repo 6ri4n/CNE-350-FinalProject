dfa7d44ba8a1862a133bec87df87f9504b3b5414
brian <bri4n.huang@gmail.com>
2023-02-22 16:23:27 -0800

    update main file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
+import time
 
 def main():
     pygame.mixer.init()
-    sound = pygame.mixer.Sound('drink-water.mp3')
-    playing = sound.play()
-    while playing.get_busy():
-        continue
+    pygame.mixer.music.load('drink-water.mp3')
+    pygame.mixer.music.play()
+    time.sleep(5)
+    pygame.mixer.music.stop()
 
 if __name__ == '__main__':
     main()
